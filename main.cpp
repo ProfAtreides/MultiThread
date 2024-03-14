fed2aa6462a9b10c3dca45e97e2655c18dc4d98f
Dave <dambrozewicz7@gmail.com>
2024-03-14 14:50:58 +0100

    Feature#1 Fixed issues with thread starvation, added a really simple but inefficient way to manage time of philosophers activities

--- main.cpp
+++ main.cpp
@@ -3,128 +3,142 @@
 #include <chrono>
 #include <random>
 #include <cstdlib>
+#include <utility>
+#include <mutex>
+#include <windows.h>
+
 using namespace std;
 
-bool free_forks[2000];
+atomic_bool free_forks[2000] = {true, true, true, true, true, true, true, true, true, true};
+mutex forks[2000];
 
-bool start = false;
-
-struct philo
-{
-    philo()
-    {
-        status ="ded";
+struct philo {
+    philo() {
+        status = "ded";
+        time = -1;
     }
-    thread t;
-    char* status;
+
+    philo(string status, int index, int leftForkIndex, int rightForkIndex) :
+            status(std::move(status)),
+            index(index),
+            time(0),
+            left_fork_index(leftForkIndex),
+            right_fork_index(rightForkIndex) {
+    }
+
+    string status;
     int index;
     int time;
     int left_fork_index;
     int right_fork_index;
 
-    void dine(pthread_mutex_t& left, pthread_mutex_t& right)
-    {
-        while(true)
-        {
+    void dine(mutex &left, mutex &right) {
+
+        cout << "Philosopher just sat down\n";
+
+        while (true) {
             int duration = rand() % 1000 + 500;
             time = duration;
-            this->status = "is thinking";
+            status = "thinking";
             this_thread::sleep_for(chrono::milliseconds(duration));
-            status = "is hungry";
+            status = "hungry";
             time = duration;
             duration = 5000;
-            pthread_mutex_timedlock(&left,NULL);
-            pthread_mutex_timedlock(&right,NULL);
+            while(!free_forks[left_fork_index] && !free_forks[right_fork_index])
+            {
+            }
+            left.lock();
+            right.lock();
             free_forks[left_fork_index] = false;
             free_forks[right_fork_index] = false;
             duration = rand() % 1000 + 500;
-            status = "is eating";
+            status = "eating";
             time = duration;
             this_thread::sleep_for(chrono::milliseconds(duration));
-            pthread_mutex_unlock(&left);
-            pthread_mutex_unlock(&right);
+            left.unlock();
+            right.unlock();
             free_forks[left_fork_index] = true;
             free_forks[right_fork_index] = true;
         }
     }
+
+    void manageTime(int time)
+    {
+        switch (status[0]) {
+            case 't':
+                this->time-=time;
+                break;
+            case 'h':
+                this->time += time;
+                break;
+            case 'e':
+                this->time -= time;
+                break;
+        }
+
+    }
 };
 
-string number_to_string(int number)
-{
-    string parsed="";
-    while(number>0)
-    {
-        parsed+='0'+number%10;
-        number/=10;
+string number_to_string(int number) {
+    string parsed = "";
+    while (number > 0) {
+        parsed += '0' + number % 10;
+        number /= 10;
     }
-    string reversed="";
-    for (int i = parsed.length() - 1; i >= 0;i--)
-    {
-        reversed+=parsed[i];
+    string reversed = "";
+    for (int i = parsed.length() - 1; i >= 0; i--) {
+        reversed += parsed[i];
     }
     return reversed;
 }
 
-void dinner_status(philo *guests, int n)
-{
-    while(true)
-    {
-        this_thread::sleep_for(chrono::milliseconds(333));
-        system("cls"); // Assuming Unix-like systems, you may need to adjust for Windows
-
-        // Print philosopher status
-        for (int i = 0; i < n; i++)
-        {
-            cout << "Philosopher " << guests[i].index << " is " << guests[i].status << " for " << guests[i].time << " milliseconds." << endl;
+void dinner_status(philo *guests, int n) {
+    int time = 1000;
+    while (true) {
+        this_thread::sleep_for(chrono::milliseconds(time/2));
+        system("cls");
+        for (int i = 0; i < n; i++) {
+            guests[i].manageTime(time);
+            cout << "Philosopher " << guests[i].index << " is " << guests[i].status << " for " << guests[i].time
+                 << " milliseconds." << endl;
         }
-
-        // Print fork availability
         cout << "Fork availability: ";
-        for(int i = 0 ; i < n * 2; i+=2)
-        {
+        for (int i = 0; i < n * 2; i += 2) {
             cout << (free_forks[i] ? "1" : "0") << " ";
         }
         cout << endl;
-
-        // Clear console after each status update
-        this_thread::sleep_for(chrono::milliseconds(1000)); // Wait for 1 second before clearing console
+        this_thread::sleep_for(chrono::milliseconds(time/2));
         system("cls");
+
     }
 }
 
-int main()
-{
+int main() {
     int n = 5;
 
-    philo *guests = new philo[n];
-    pthread_mutex_t *forks = new pthread_mutex_t [n];
+    philo guests[5];
+    thread t[5];
 
-    for (int i = 0; i< n; i++)
-    {
-        pthread_mutex_init(&forks[i],NULL);
-        int left_index = (i+1<n) ? i: 0;
-        int right_index = (i+1<n)?i+1:i;
-        guests[i].left_fork_index = left_index;
-        guests[i].right_fork_index = right_index;
-        guests[i].index = i + 1;
-        guests[i].t = thread([&]
-                             {
-                                 guests[i].dine(forks[left_index],forks[right_index]);
-                             });
-
+    for (int i = 0; i < n; i++) {
+        int right_index = (i + 1 < n) ? i : 0;
+        int left_index = (i + 1 < n) ? i + 1 : i;
+        guests[i] = philo("ready for a feast", i + 1, left_index, right_index);
+        t[i] = thread([i, &guests] {
+            guests[i].dine(forks[guests[i].left_fork_index], forks[guests[i].right_fork_index]);
+        });
     }
 
-    thread status([&,n]{
-        dinner_status(guests,n);
+    thread status([&, n] {
+        dinner_status(guests, n);
     });
+    status.join();
 
-    for( int i =0; i< n;i++)
-    {
-        guests[i].t.join();
+    for (int i = 0; i < n; i++) {
+        t[i].join();
     }
 
-    start = true;
 
-    status.join();
+
+
 
 }
