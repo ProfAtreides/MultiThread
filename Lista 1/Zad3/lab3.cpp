29b93fd4e5f605a11de64e357953261b5ec2f188
ProfAtreides <dambrozewicz7@gmail.com>
2024-04-10 01:24:22 +0200

    Hotfix #3 Modified the smokerProcess function to emphasize semaphore not being locked by a single thread

--- Lista 1/Zad3/lab3.cpp
+++ Lista 1/Zad3/lab3.cpp
@@ -13,7 +13,7 @@ sem_t matchboxSem;
 
 mutex consoleMutex;
 
-void displayMessage(const string& message) {
+void displayMessage(const string &message) {
     unique_lock<mutex> lock(consoleMutex);
     cout << message + "\n";
 }
@@ -21,34 +21,35 @@ void displayMessage(const string& message) {
 void smokerProcess(string name) {
     random_device rd;
     mt19937 gen(rd());
+    uniform_int_distribution<> act(50, 250);
     uniform_int_distribution<> nap(1, 5);
     while (true) {
         displayMessage(name + " is waiting for grinder");
         sem_wait(&grinderSem);
         displayMessage(name + " is whipping up a pipe");
         sem_post(&grinderSem);
+        this_thread::sleep_for(chrono::milliseconds (act(gen)));
         displayMessage(name + " is waiting for matches");
         sem_wait(&matchboxSem);
         displayMessage(name + " fires up his pipe");
         sem_post(&matchboxSem);
+        this_thread::sleep_for(chrono::milliseconds (act(gen)));
         displayMessage(name + " finishes smoking and lays down for a nap");
         this_thread::sleep_for(chrono::seconds(nap(gen)));
     }
 }
 
 int main() {
-    int m = 5;
-    int g = 5;
+    int m = 1,g=1,n=5;
 
     sem_init(&grinderSem, 0, g);
     sem_init(&matchboxSem, 0, m);
 
-    int n = 5;
-    string name[] = {"Alice", "Bob", "Charlie", "David", "Eve"};
+    string name[] = {"Micheal", "Pam", "Dwight", "Jim", "Stanley", "Angela", "Kevin", "Oscar", "Creed"};
 
-    thread* smokers =  new thread[n];
+    thread *smokers = new thread[n];
     for (int i = 0; i < n; ++i) {
-        smokers[i] = thread(smokerProcess,name[i]);
+        smokers[i] = thread(smokerProcess, name[i]);
     }
 
     // Oczekiwanie na zakończenie wątków
